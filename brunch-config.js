fc9a7b7defd310c68138289f9178c25d2613b450
Paul Miller <paul@paulmillr.com>
2016-03-28 06:30:09 +0300

    Update brunch-config.js

--- brunch-config.js
+++ brunch-config.js
@@ -1,22 +1,14 @@
 module.exports = {
   npm: {
-    styles: {
-      'normalize.css': ['normalize.css']
-    }
+    styles: {'normalize.css': ['normalize.css']}
   },
 
   plugins: {
-    babel: {
-      presets: ['es2015', 'react'],
-    }
+    babel: {presets: ['es2015', 'react']}
   },
 
   files: {
-    javascripts: {
-      joinTo: 'app.js'
-    },
-    stylesheets: {
-      joinTo: 'app.css'
-    }
+    javascripts: {joinTo: 'app.js'},
+    stylesheets: {joinTo: 'app.css'}
   }
 };
